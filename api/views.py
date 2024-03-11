62b84327475cbf815be17ff047d02c3263eba22f
Neon16bit <151030187+neon16bit@users.noreply.github.com>
2024-03-12 02:28:06 +0600

    more fixes to views

--- api/views.py
+++ api/views.py
@@ -5,7 +5,6 @@ def course_list(request):
     courses = Course.objects.all()
     return render(request, 'course_list.html', {'courses': courses})
 
-
 def curriculum_list(request):
     curriculums = Curriculum.objects.all()
     return render(request, 'curriculum_list.html', {'curriculums': curriculums})
@@ -17,3 +16,9 @@ def room_list(request):
 def faculty_member_list(request):
     faculty_members = faculty_member.objects.all()
     return render(request, 'faculty_member_list.html', {'faculty_members': faculty_members})
+
+
+
+# api views
+from rest_framework import serializers, viewsets
+# TODO: Make serializers for each model
