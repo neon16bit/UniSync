159c46fd949d683bf457a43f72247d02b6226a9a
Neon16bit <151030187+neon16bit@users.noreply.github.com>
2024-03-12 21:12:55 +0600

    fixes to api models

--- api/models.py
+++ api/models.py
@@ -26,21 +26,8 @@ class Curriculum(models.Model):
     ]
     
     curriculum_semester = models.CharField(max_length=3, unique=True, primary_key=True, choices=SEMESTERS)
-    class Curriculum(models.Model):
-        SEMESTERS = [
-            ('1.1', '1st Year 1st Semester'),
-            ('1.2', '1st Year 2nd Semester'),
-            ('2.1', '2nd Year 1st Semester'),
-            ('2.2', '2nd Year 2nd Semester'),
-            ('3.1', '3rd Year 1st Semester'),
-            ('3.2', '3rd Year 2nd Semester'),
-            ('4.1', '4th Year 1st Semester'),
-            ('4.2', '4th Year 2nd Semester'),
-        ]
-        
-        curriculum_semester = models.CharField(max_length=3, unique=True, primary_key=True, choices=SEMESTERS)
-        # TODO: Fix this bs
-        courses = models.ManyToManyField(Course)
+    # TODO: Fix this bs
+    courses = models.ManyToManyField(Course,)
     
            
 class faculty_member(models.Model):
