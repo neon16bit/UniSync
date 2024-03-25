1becbe96464438f874d0267f7ea5f1505e0897a1
Neon16bit <151030187+neon16bit@users.noreply.github.com>
2024-03-26 00:59:41 +0600

    add .env and decouple config settings

--- UniSyncBackend/settings.py
+++ UniSyncBackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!_^^j37@p_7tba0p18)&$k+*3*@@vb9xlryacr3onl+%br0j!z'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=lambda hosts: [host.strip() for host in hosts.split(',')])
 
 
 # Application definition
